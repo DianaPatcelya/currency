762ec40e7067e99adfac61f38d6d9d6665995c8f
Diana <djdiasya2014@gmail.com>
2023-11-24 22:38:05 +0200

    HW7

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,31 +1,21 @@
-from django.http.response import HttpResponse
+from django.shortcuts import render
 
 from currency.models import Rate, ContactUs
 
 
 def rate_list(request):
-
-    results = []
     rates = Rate.objects.all()
+    context = {
+        'rates': rates
+    }
 
-    for rate in rates:
-        results.append(
-            f'ID: {rate.id}, buy: {rate.buy}, sell: {rate.sell}, type: {rate.type}, '
-            f'source: {rate.source}, created: {rate.created} <br>'
-        )
-
-    return HttpResponse(str(results))
+    return render(request, 'rate_list.html', context)
 
 
 def contactus_list(request):
-
-    results = []
     contacts = ContactUs.objects.all()
+    context = {
+        'contacts': contacts
+    }
 
-    for contact in contacts:
-        results.append(
-            f'ID: {contact.id}, Email: {contact.email_from}, '
-            f'Subject: {contact.subject}, Message: {contact.message}'
-        )
-
-    return HttpResponse(str(results))
+    return render(request, 'contactus_list.html', context)
