6dcc1e01f7292aef90637a269dafc942dab19423
Diana <djdiasya2014@gmail.com>
2023-12-04 17:32:57 +0200

    HW9

--- app/currency/forms.py
+++ app/currency/forms.py
@@ -1,6 +1,18 @@
 from django import forms
 
-from currency.models import Source
+from currency.models import Source, Rate, ContactUs
+
+
+class RateForm(forms.ModelForm):
+
+    class Meta:
+        model = Rate
+        fields = (
+            'buy',
+            'sell',
+            'currency_type',
+            'source',
+        )
 
 
 class SourceForm(forms.ModelForm):
@@ -10,4 +22,15 @@ class SourceForm(forms.ModelForm):
         fields = (
             'source_url',
             'name',
+        )
+
+
+class ContactUsForm(forms.ModelForm):
+
+    class Meta:
+        model = ContactUs
+        fields = (
+            'email_from',
+            'subject',
+            'message'
         )
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,23 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from currency.views import (
-    rate_list,
-    contactus_list,
-    source_list,
-    source_create,
-    source_update,
-    source_delete,
-    source_details
-)
+from currency.views import IndexView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rate/list/', rate_list),
-    path('contactus/list/', contactus_list),
-    path('source/list/', source_list),
-    path('source/create/', source_create),
-    path('source/update/<int:pk>/', source_update),
-    path('source/delete/<int:pk>/', source_delete),
-    path('source/details/<int:pk>/', source_details),
+
+    path('currency/', include('currency.urls')),
+
+    path('__debug__/', include('debug_toolbar.urls')),
+
+    path('', IndexView.as_view()),
 ]
